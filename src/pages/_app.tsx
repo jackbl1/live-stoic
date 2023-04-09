d577a7161e7c23ce3c91ac5fe26037c5bdebd839
Jack Bloomfeld <jackbloomfeld1@gmail.com>
2023-04-09 18:39:20 -0400

    Added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import { MetamaskProvider } from "../hooks/useMetamask";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MetamaskProvider>
-      <SessionProvider session={pageProps.session} refetchInterval={0}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </MetamaskProvider>
+    <>
+      <MetamaskProvider>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MetamaskProvider>
+      <Analytics />
+    </>
   );
 }
 
